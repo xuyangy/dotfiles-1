15e8e34e0b8e2bd1115059564215e5b2105c5aac
Artur Tagisow <atagisov@gmail.com>
2019-09-27 14:08:54 +0200

    Add setup.sh script with fzf setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
 ln -sf ~/.vimrc ~/dotfiles/.vimrc
 ln -sf ~/.bashrc ~/dotfiles/.bashrc
 ln -sf ~/.bash_aliases ~/dotfiles/.bash_aliases

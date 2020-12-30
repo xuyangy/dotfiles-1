4c96592f928d2d8dc561f740fd6241965f482ab4
Artur Tagisow <atagisov@protonmail.com>
2020-12-30 04:25:16 +0100

    bump setup script

--- setup.sh
+++ setup.sh
@@ -8,7 +8,8 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 ln -s -f ~/dotfiles/init.vim ~/.config/nvim/init.vim
 ln -s -f ~/dotfiles/.bashrc ~/.bashrc 
 ln -s -f ~/dotfiles/.zshrc ~/.bashrc 
+ln -s -f ~/dotfiles/.tmux.conf ~/.tmux.conf 
 ln -s -f ~/dotfiles/.bash_aliases ~/.bash_aliases 
 ln -s -f ~/dotfiles/.gitconfig ~/.gitconfig 
 
-nvm install 12
+nvm install 14

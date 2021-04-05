36783ba2430ee1422013b0ab142ebf8a9f7b949d
Artur Tagisow <artur.tagisow@billennium.com>
2021-04-05 10:05:03 +0200

    add dark mode for qtbrowser

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,3 +1,6 @@
 config.load_autoconfig()
 config.bind('<Ctrl-a>', 'fake-key <Home>', mode='insert')
 config.bind('<Ctrl-Shift-a>', 'fake-key <Ctrl-a>', mode='insert') # select all
+
+config.set("colors.webpage.darkmode.enabled", True)
+config.set("colors.webpage.bg", "black")

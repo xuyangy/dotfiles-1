2c484fa0857775b41d14f74f40b478706983c88a
Artur Tagisow <artur.tagisow@billennium.com>
2021-04-07 20:57:33 +0200

    we lua now

---
+++ .config/nvim/lua/3nuc/mappings.lua
@@ -0,0 +1,43 @@
+local options = { noremap = true }
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>w', options)
+
+vim.api.nvim_set_keymap('n','<C-j>', '<C-w>w', options)
+vim.api.nvim_set_keymap('n','<C-k>', '<C-w>W', options)
+vim.api.nvim_set_keymap('n','<leader>q', ':q<cr>', options)
+vim.api.nvim_set_keymap('n','<leader>w', ':w<cr>', options)
+vim.api.nvim_set_keymap('n','<leader>ve', ':e $MYVIMRC<cr>', options)
+vim.api.nvim_set_keymap('n','<down>', ':cn<cr>', options)
+vim.api.nvim_set_keymap('n','<up>', ':cp<cr>', options)
+
+
+vim.api.nvim_set_keymap('n','<leader>ll', ':lua vim.lsp.stop_client(vim.lsp.get_active_clients())<cr>:edit<cr>', options)
+vim.api.nvim_set_keymap('t','<esc>', '<C-\\><C-N>', options)
+vim.api.nvim_set_keymap('n', '<leader>b', '<cmd>Telescope git_branches<cr>', options)
+vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope git_files<cr>', options)
+vim.api.nvim_set_keymap( 'n', '<leader>fg', '<cmd>Telescope live_grep<cr>', options)
+vim.api.nvim_set_keymap( 'n', '<leader>fs', '<cmd>Telescope lsp_document_symbols<cr>', options)
+vim.api.nvim_set_keymap( 'n', '<leader>fS', '<cmd>Telescope lsp_document_symbols<cr>', options)
+
+vim.api.nvim_set_keymap( 'n', '<leader>fd', '<cmd>Telescope lsp_document_diagnostics<cr>', options)
+vim.api.nvim_set_keymap( 'n', '<leader>fD', '<cmd>Telescope lsp_workspace_diagnostics<cr>', options)
+
+vim.api.nvim_set_keymap( 'n', '<leader>fG', ':Telescope grep_string search=<C-R><C-W><cr>', options)
+
+vim.api.nvim_set_keymap( 'n', '<leader>fF', '<cmd>Telescope find_files<cr>', options)
+vim.api.nvim_set_keymap( 'n', '<leader>fS', '<cmd>Telescope lsp_workspace_symbols<cr>', options)
+vim.api.nvim_set_keymap( 'n', '<leader>fo', '<cmd>Telescope oldfiles<cr>', options)
+vim.api.nvim_set_keymap( 'n', '<leader>fb', '<cmd>Telescope buffers<cr>', options)
+vim.api.nvim_set_keymap( 'i', '<silent><expr> <C-Space>', 'compe#complete()', options)
+vim.api.nvim_set_keymap( 'i', '<silent><expr> <CR>', 'compe#confirm(\'<CR>\')', options)
+vim.api.nvim_set_keymap( 'i', '<silent><expr> <C-e>', 'compe#close(\'<C-e>\')', options)
+vim.api.nvim_set_keymap( 'n', '<leader>s', ':G<CR>', options)
+vim.api.nvim_set_keymap( 'n', '<leader>ggp', ':G -c push.default=current push<CR>' , options)
+vim.api.nvim_set_keymap( 'n', '<leader>gL', ':G pull', options)
+vim.api.nvim_set_keymap( 'n', '<leader>gcb', ':G checkout origin/develop -b' , options)
+vim.api.nvim_set_keymap( 'n', '<leader>glo', ':G log<cr>', options)
+vim.api.nvim_set_keymap( 'n', '<leader>glO', ':G log %<cr>', options)
+vim.api.nvim_set_keymap( 'n', '<leader>gf', ':G fetch<cr>', options)
+vim.api.nvim_set_keymap( 'n', '<silent>w', '<Plug>CamelCaseMotion_w', options)
+vim.api.nvim_set_keymap( 'n', '<silent>b', '<Plug>CamelCaseMotion_b', options)
+vim.api.nvim_set_keymap( 'n', '<leader>p', ':RnvimrToggle<cr>', options)
+vim.api.nvim_set_keymap( 'n', '<leader>P', ':RnvimrResize<cr>', options)
--- .config/nvim/lua/3nuc/plugins.lua
+++ .config/nvim/lua/3nuc/plugins.lua
@@ -1,124 +1,34 @@
-syntax enable 
-filetype plugin indent on
-    
-set termguicolors
-set nocompatible rnu nu tabstop=2 shiftwidth=2 expandtab 
-set ruler wildmenu noswapfile autoread
-set ignorecase smartcase
-set hidden
-set completeopt=menuone,noselect
-set colorcolumn=9999
+vim.cmd 'packadd paq-nvim'
+local paq = require('paq-nvim').paq
+  paq {'savq/paq-nvim', opt=true }
+  paq 'neovim/nvim-lspconfig'
+  paq 'nvim-lua/completion-nvim'
+  paq 'tjdevries/nlua.nvim'
+  paq 'tjdevries/lsp_extensions.nvim'
+  paq 'nvim-treesitter/nvim-treesitter'
+  paq 'hoob3rt/lualine.nvim'
+  paq 'nvim-lua/popup.nvim'
+  paq 'nvim-lua/plenary.nvim'
+  paq 'kyazdani42/nvim-web-devicons'
+  paq { 'nvim-telescope/telescope-fzy-native.nvim', hook='git submodule update --init --recursive' } -- https://github.com/savq/paq-nvim/issues/6
+  paq 'nvim-telescope/telescope.nvim'
+  paq 'hrsh7th/nvim-compe'
+  paq 'hrsh7th/vim-vsnip'
+  paq 'windwp/nvim-ts-autotag'
+  paq 'lewis6991/gitsigns.nvim'
+  paq 'tpope/vim-fugitive'
+  paq 'kevinhwang91/rnvimr'
+  paq 'bkad/CamelCaseMotion'
+  paq 'tpope/vim-surround'
+  paq 'jiangmiao/auto-pairs'
+  paq 'christianchiarulli/nvcode-color-schemes.vim'
+  paq 'Yggdroot/indentLine'
+  paq 'lukas-reineke/indent-blankline.nvim'
 
-" https://vim.fandom.com/wiki/Highlight_current_line
-augroup CursorLine
-  au!
-  au VimEnter,WinEnter,BufWinEnter * setlocal cursorline
-  au WinLeave * setlocal nocursorline
-augroup END
 
-set shell=/bin/bash " zsh slow with vim-fugitive :Gstatus (on WSL)
-set omnifunc=v:lua.vim.lsp.omnifunc "felt cute may delete later <C-x><C-o> remember
-nmap <leader>ll :lua vim.lsp.stop_client(vim.lsp.get_active_clients())<cr>:edit<cr>
-
-nmap <C-j> <C-w>w
-nmap <C-k> <C-w>W
-nmap <leader>q :q<cr>
-nmap <leader>w :w<cr>
-nmap <leader>ve :e $MYVIMRC<cr>
-nmap <down> :cn<cr>
-nmap <up> :cp<cr>
-
-tmap <Esc> <C-\><C-N>
-autocmd! bufwritepost $MYVIMRC source $MYVIMRC " autoreload vimrc on save
-autocmd FileType gitcommit exec 'au VimEnter * startinsert' 
-
-" vim cursor shape not changing in tmux
-if exists('$TMUX')
-  let &t_SI = "\<Esc>Ptmux;\<Esc>\e[5 q\<Esc>\\"
-  let &t_EI = "\<Esc>Ptmux;\<Esc>\e[2 q\<Esc>\\"
-else
-  let &t_SI = "\e[5 q"
-  let &t_EI = "\e[2 q"
-endif
-
-call plug#begin()
-  Plug 'neovim/nvim-lspconfig'
-  Plug 'nvim-lua/completion-nvim'
-    set completeopt=menuone,noinsert,noselect
-  Plug 'tjdevries/nlua.nvim'
-  Plug 'tjdevries/lsp_extensions.nvim'
-
-  Plug 'nvim-treesitter/nvim-treesitter'
-
-  Plug 'hoob3rt/lualine.nvim'
-
-  Plug 'nvim-lua/popup.nvim'
-  Plug 'nvim-lua/plenary.nvim'
-  Plug 'kyazdani42/nvim-web-devicons'
-  Plug 'nvim-telescope/telescope-fzy-native.nvim'
-  Plug 'nvim-telescope/telescope.nvim'
-    nnoremap <leader>b <cmd>Telescope git_branches<cr>
-    nnoremap <leader>ff <cmd>Telescope git_files<cr>
-    nnoremap <leader>fg <cmd>Telescope live_grep<cr>
-    nnoremap <leader>fs <cmd>Telescope lsp_document_symbols<cr>
-    nnoremap <leader>fS <cmd>Telescope lsp_document_symbols<cr>
-
-    nnoremap <leader>fd <cmd>Telescope lsp_document_diagnostics<cr>
-    nnoremap <leader>fD <cmd>Telescope lsp_workspace_diagnostics<cr>
-
-    nnoremap <leader>fG :Telescope grep_string search=<C-R><C-W><cr>
-
-    nnoremap <leader>fF <cmd>Telescope find_files<cr>
-    nnoremap <leader>fS <cmd>Telescope lsp_workspace_symbols<cr>
-    nnoremap <leader>fo <cmd>Telescope oldfiles<cr>
-    nnoremap <leader>fb <cmd>Telescope buffers<cr>
-
-  "misc
-  Plug 'hrsh7th/nvim-compe'
-    Plug 'hrsh7th/vim-vsnip'
-    inoremap <silent><expr> <C-Space> compe#complete()
-    inoremap <silent><expr> <CR>      compe#confirm('<CR>')
-    inoremap <silent><expr> <C-e>     compe#close('<C-e>')
-    inoremap <silent><expr> <C-f>     compe#scroll({ 'delta': +4 })
-    inoremap <silent><expr> <C-d>     compe#scroll({ 'delta': -4 })
-  Plug 'windwp/nvim-ts-autotag'
-  Plug 'lewis6991/gitsigns.nvim'
-  Plug 'tpope/vim-fugitive'
-    nmap <leader>s :G<CR>
-    " don't ask to set upstream
-    nmap <leader>ggp :G -c push.default=current push<CR> 
-    nmap <leader>gL :G pull
-    nmap <leader>gcb :G checkout origin/develop -b 
-    nmap <leader>glo :G log<cr>
-    nmap <leader>glO :G log %<cr>
-    nmap <leader>gf :G fetch<cr>
-  Plug 'kevinhwang91/rnvimr'
-    nmap <leader>p :RnvimrToggle<cr>
-    nmap <leader>P :RnvimrResize<cr>
-    let g:rnvimr_enable_ex = 1
-    let g:rnvimr_action = {
-            \ '<CR>': 'NvimEdit drop'
-            \ }
-  Plug 'bkad/CamelCaseMotion'
-   " https://github.com/bkad/CamelCaseMotion
-   " nnoremap <Space> <NOP>
-    map <silent> w <Plug>CamelCaseMotion_w
-    map <silent> b <Plug>CamelCaseMotion_b
-    sunmap w
-    sunmap b
-
-  Plug 'tpope/vim-surround'
-  Plug 'jiangmiao/auto-pairs'
-  Plug 'christianchiarulli/nvcode-color-schemes.vim'
-  Plug 'Yggdroot/indentLine'
-  Plug 'lukas-reineke/indent-blankline.nvim'
-call plug#end()
-
-colorscheme nvcode
-
-lua << EOF
 local actions = require('telescope.actions')
-require('telescope').setup{
+local telescope = require('telescope')
+telescope.setup{
   defaults = {
     file_sorter = require('telescope.sorters').get_fzy_sorter,
     color_devicons = true,
@@ -137,11 +47,7 @@ require('telescope').setup{
   }
 }
 
-require('telescope').load_extension('fzy_native')
-EOF
-
-
-lua << EOF
+telescope.load_extension('fzy_native')
 require'nvim-treesitter.configs'.setup {
   autotag = {
     enable = true,
@@ -151,9 +57,6 @@ require'nvim-treesitter.configs'.setup {
     enable = true,
   }
 }
-EOF
-
-lua << EOF
 local nvim_lsp = require('lspconfig')
 local on_attach = function(client, bufnr)
   local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
@@ -277,9 +180,7 @@ nvim_lsp.diagnosticls.setup{
     }
   }
 }
-EOF
 
-lua << EOF
 require'compe'.setup {
   enabled = true;
   autocomplete = true;
@@ -308,14 +209,8 @@ require'compe'.setup {
   };
 }
 
-EOF
-
-lua << EOF
 require('gitsigns').setup()
 
-EOF
-
-lua << EOF
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics, {
     virtual_text = true,
@@ -324,9 +219,7 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     update_in_insert = true,
   }
 )
-EOF
 
-lua << EOF
 require('lualine').setup{
   options = {
     theme = 'powerline',
@@ -340,7 +233,3 @@ require('lualine').setup{
      lualine_z = {}
   }
 }
-EOF
-
-highlight Normal ctermbg=black
-highlight Normal guibg=black

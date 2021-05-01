27bf4c837adc793a249e2c3afd7e0d922e9f38a0
Artur Tagisow <artur.tagisow@billennium.com>
2021-05-01 17:21:14 +0200

    add cpu temp to status bar (thinkpad t430 specific)

--- .config/sway/status.sh
+++ .config/sway/status.sh
@@ -1,7 +1,8 @@
 while 
-  date=$( date +'%Y-%m-%d %k:%M:%S' )
+  cpu_temp=$(cat /proc/acpi/ibm/thermal | grep -oP "\d+" | head -1)
   battery_capacity=$( cat /sys/class/power_supply/BAT0/capacity )
-  echo $battery_capacity $date 
+  date=$( date +'%Y-%m-%d %k:%M:%S' )
+  echo T:$cpu_temp B:$battery_capacity $date 
 do sleep 1; 
 done
 

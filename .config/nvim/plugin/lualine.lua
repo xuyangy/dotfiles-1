99b8430217377c1c957fc1c2a98b12bbcb60384f
Artur Tagisow <artur.tagisow@billennium.com>
2021-06-18 22:08:13 +0200

    idk

--- .config/nvim/plugin/lualine.lua
+++ .config/nvim/plugin/lualine.lua
@@ -1,20 +1,33 @@
 require 'paq-nvim'.paq 'hoob3rt/lualine.nvim'
 
+
 require('lualine').setup{
   options = {
-    theme = 'powerline',
+    theme = 'tokyonight',
+    section_separators = '',
+    component_separators = '',
   },
   sections = {
     lualine_a = {'mode'},
     lualine_b = {},
-    lualine_c = {{'filename', path = 1},{'diagnostics', sources = {'nvim_lsp'}, color_error = "#ff0000"},'location', 'progress'},
+    lualine_c = {
+      {
+        'filename', 
+        path=1
+      },
+      {
+        'diagnostics', 
+        sources = {'nvim_lsp'}, 
+      }, 
+      'location', 
+      'progress'
+    },
     lualine_x = {},
-    lualine_y = {'branch', 'diff'},
-    lualine_z = {}
+    lualine_y = {},
+    lualine_z = {'fugitive_branch'}
   },
   inactive_sections = {
-
     lualine_y = {'branch', 'diff'},
-  }
+  },
 }
 

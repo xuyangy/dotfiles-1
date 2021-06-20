7fa5a0adf0533c54a55ab3746eeeef24d0ea5f98
Artur Tagisow <artur.tagisow@billennium.com>
2021-06-20 17:28:13 +0200

    idk lmao

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,3 @@
-require('sets')
-require('maps')
-
 vim.cmd 'packadd paq-nvim'
 Paq = require('paq-nvim').paq
 local plugins = {
@@ -18,3 +15,28 @@ for _,plugin in ipairs(plugins) do
 end
 
 vim.cmd('runtime macros/sandwich/keymap/surround.vim')
+
+
+vim.cmd('syntax enable')
+vim.cmd('filetype plugin indent on')
+
+vim.g.mapleader = " "
+vim.opt.shell = '/bin/bash' -- zsh starts too slow for fugitive, but also term:// starts bash instead of zsh
+vim.opt.termguicolors= true
+vim.opt.ruler=         true
+vim.opt.wildmenu=      true
+vim.opt.autoread=      true
+vim.opt.ignorecase=    true
+vim.opt.smartcase=     true
+vim.opt.hidden=        true
+vim.opt.completeopt=   'menuone,noinsert,noselect'
+vim.opt.cursorline=    true
+
+vim.opt.number=        true
+vim.opt.relativenumber=true
+
+vim.opt.tabstop=       2
+vim.opt.shiftwidth=    2
+vim.opt.shiftround=    true
+vim.opt.expandtab=     true
+vim.opt.swapfile=     false

98d49473188aa878f4dda7f257a12f9555ef83fb
Artur Tagisow <artur@tagisow.dev>
2021-09-08 18:48:34 +0200

    fix mixed up ca and ce - thanks Tim

--- .config/nvim/ftplugin/fugitive.lua
+++ .config/nvim/ftplugin/fugitive.lua
@@ -1,6 +1,6 @@
 local opts = {noremap = true, silent = true};
 local current_buffer = 0;
 vim.api.nvim_buf_set_keymap(current_buffer, 'n', 'cc', '<cmd>Git commit --quiet<CR>', opts)
-vim.api.nvim_buf_set_keymap(current_buffer, 'n', 'ce', '<cmd>Git commit --quiet --amend<CR>', opts)
-vim.api.nvim_buf_set_keymap(current_buffer, 'n', 'ca', '<cmd>Git commit --quiet --amend --no-edit<CR>', opts)
+vim.api.nvim_buf_set_keymap(current_buffer, 'n', 'ca', '<cmd>Git commit --quiet --amend<CR>', opts)
+vim.api.nvim_buf_set_keymap(current_buffer, 'n', 'ce', '<cmd>Git commit --quiet --amend --no-edit<CR>', opts)
 

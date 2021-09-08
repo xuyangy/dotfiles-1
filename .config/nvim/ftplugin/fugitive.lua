16e9ffb5cff2114c3addb25245c521fbcb77bffe
Artur Tagisow <artur@tagisow.dev>
2021-09-08 10:01:53 +0200

    more pure lua

--- .config/nvim/ftplugin/fugitive.lua
+++ .config/nvim/ftplugin/fugitive.lua
@@ -1,4 +1,6 @@
-vim.api.nvim_command([[nnoremap <buffer> <silent> cc :Git commit --quiet<CR>]])
-vim.api.nvim_command([[nnoremap <buffer> <silent> ca :Git commit --quiet --amend<CR>]])
-vim.api.nvim_command([[nnoremap <buffer> <silent> ce :Git commit --quiet --amend --no-edit<CR>]])
-    
+local opts = {noremap = true, silent = true};
+local current_buffer = 0;
+vim.api.nvim_buf_set_keymap(current_buffer, 'n', 'cc', '<cmd>Git commit --quiet<CR>', opts)
+vim.api.nvim_buf_set_keymap(current_buffer, 'n', 'ce', '<cmd>Git commit --quiet --amend<CR>', opts)
+vim.api.nvim_buf_set_keymap(current_buffer, 'n', 'ca', '<cmd>Git commit --quiet --amend --no-edit<CR>', opts)
+

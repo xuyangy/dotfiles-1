49eae7e561864317d013e907cf788c6356810c9d
Artur Tagisow <artur@tagisow.dev>
2021-09-12 22:13:15 +0200

    fix dap

--- .config/nvim/plugin/dap.lua
+++ .config/nvim/plugin/dap.lua
@@ -4,25 +4,30 @@ local dap = require('dap')
 dap.adapters.firefox = {
   type = 'executable',
   command = 'node',
-  args = {os.getenv('HOME') .. '/dap/firefox-adapter.js'},
+  args = {os.getenv('HOME') .. '/dap/firefox/adapter.bundle.js'},
 }
-dap.configurations.vue = {
-  {
+dap.set_log_level('TRACE')
+
+local config = {
+    name = 'Debug with Firefox',
     type = 'firefox',
     request = 'launch',
-    program = '${file}',
-    cwd = vim.fn.getcwd(),
-    sourceMaps = true,
-    protocol = 'inspector',
-    console = 'integratedTerminal',
-    firefoxExecutable = '/usr/bin/firefox-nightly',
-    url = 'http://localhost:3333',
-    webRoot = '${workspaceFolder}'
+    reAttach = true,
+    url = 'http://localhost:3000',
+    webRoot = '${workspaceFolder}',
+    --[[ pathMappings = {
+      {
+        url = 'http://localhost:3000',
+        path = "${workspaceFolder}"
+      }
+    }, ]]
+    firefoxExecutable = '/usr/bin/firefox-nightly'
   }
-}
+
+dap.configurations.vue = { config }
+dap.configurations.typescript = { config }
 
 nnoremap {'\\b', dap.toggle_breakpoint}
 nnoremap {'\\c', dap.continue}
-nnoremap {'\\o', dap.setp_over}
+nnoremap {'\\o', dap.step_over}
 nnoremap {'\\i', dap.step_into}
-nnoremap {'\\r', dap.repl_open}
